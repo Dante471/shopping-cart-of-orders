25e7b185e77f2b77a841debbd0f2c6427f1ef0fb
dan-d <dan-dannn@yandex.ru>
2021-11-29 20:30:24 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/shoppingcartoforders/ShoppingCartOfOrdersApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.shoppingcartoforders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartOfOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartOfOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/shoppingcartoforders/ShoppingCartOfOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.shoppingcartoforders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartOfOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
